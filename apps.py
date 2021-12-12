3ba83d284f747dc42e897611b99ae7c4388aab04
anveshreddy725 <54026990+anveshreddy725@users.noreply.github.com>
2021-12-12 15:15:18 -0700

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SolarpvConfig(AppConfig):
+class ExampleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'solarpv'
+    name = 'example'
