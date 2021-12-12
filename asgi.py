501102da55635f09af6524e3b63ed3de18121c12
anveshreddy725 <54026990+anveshreddy725@users.noreply.github.com>
2021-12-12 15:14:50 -0700

    Add files via upload

--- asgi.py
+++ asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_solarpv project.
+ASGI config for courses project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_solarpv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courses.settings')
 
 application = get_asgi_application()
